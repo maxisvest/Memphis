a01a48ccaf7eda4534ad9a3101cf1474e8d8e985
yang.yu <1447829379@qq.com>
2021-04-01 18:31:58 +0800

    first commit

---
+++ Memphis-core/src/main/java/com/github/maxisvest/Test.java
@@ -0,0 +1,8 @@
+package com.github.maxisvest;
+
+/**
+ * Create by yuyang
+ * 2021/4/1 18:19
+ */
+public class Test {
+}
---
+++ Memphis-starter/src/main/java/com/github/maxisvest/Test.java
@@ -0,0 +1,8 @@
+package com.github.maxisvest;
+
+/**
+ * Create by yuyang
+ * 2021/4/1 18:18
+ */
+public class Test {
+}
